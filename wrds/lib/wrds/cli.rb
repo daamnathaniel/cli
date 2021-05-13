8c603bf25037184f3566803ff586ed14c6540efc
Nathaniel Adam <daamnathaniel@gmail.com>
2021-05-13 01:55:33 -0500

    backup

--- wrds/lib/wrds/cli.rb
+++ wrds/lib/wrds/cli.rb
@@ -1,3 +1,6 @@
+require 'highline/import'
+
+
 
 module Wrds
 
@@ -5,19 +8,27 @@ module Wrds
     %( 0.1.0 )
   end
 
-  def title
+  def self.title
     %( wrds )
   end
 
-  def slogan
+  def self.slogan
     %( wrds: find words. )
   end
 
-  def please_choose
+  def self.space
+    %(
+
+
+
+    )
+  end
+
+  def self.please_choose
     %( please choose: )
   end
 
-  def instructions 
+  def self.instructions 
     %(  
     In order to find [words] that [modify/relate_to] [word],
     you just need to type [modifier] ['word'].
@@ -29,13 +40,47 @@ module Wrds
   end
 end
 
+  class Display
+    extend Wrds
+
+    def self.text(list)
+      "#{list}"
+    end
+
+    def self.main(list)
+      puts "#{list}"
+    end
+
+    def self.options(list)
+      list.each_pair {|k,v| puts "#{k}"}
+    end
+  end
 
 
 
+  class Menu
+    Display.options()
 
-class Wrd_Methods < OpenStruct; end
 
-Wrd_Methods = Wrd_Methods.new(    
+
+wrd_key = Proc.new {|k,v| puts k}
+wrd_methods = OpenStruct.new(
+  related_to: :ml, sound_like: :sl, spelled_like: :sp, 
+    nouns_often_described_by: :rel_jja, adjectives_often_describing: :rel_jjb, 
+    related_synonyms: :rel_syn, related_triggers: :rel_trg,
+    related_antonyms: :rel_ant, kind_of_hyponyms: :rel_spc, 
+    general_hyponyms: :rel_gen, comporomise_hyponyms: :rel_com,  
+    frequent_followers: :rel_bga, frequent_predecessors: :rel_bgb, 
+    related_rhymes: :rel_rhy, approximate_rhymes: :rel_nry,
+    related_homophones: :rel_hom, consonant_match: :rel_cns,
+    part_of_meronyms: :rel_par
+)
+
+
+
+# class Wrd_Methods < OpenStruct; end
+
+Wrd_Methods = OpenStruct.new(    
     mean_like: :ml, sound_like: :sl, spelled_like: :sp, 
     related_popular_nouns: :rel_jja, related_popular_adjectives: :rel_jjb, 
     related_synonyms: :rel_syn, related_triggers: :rel_trg,
@@ -49,22 +94,101 @@ Wrd_Methods = Wrd_Methods.new(
 
 
 
-class Display
-  extend Wrds
 
-  def self.text(list)
-      "#{list}"
+
+class Datamuse 
+  attr_accessor :damu
+  def initialize 
+    @damu = Blanket::wrap("https://api.datamuse.com")
   end
-
-  def self.main(list)
-    puts "#{list}"
-  end
-
-  def self.options(list)
-    list.each_pair {|k,v| puts "#{k}"}
+  def damu
+    @damu
   end
 end
 
+
+
+class Sug
+  def initialize(input)
+    @input = input
+  end
+  def suggestion
+    Datamuse.new.damu.send('sug').get(params: {s: "#{@input}"})
+  end
+end
+
+
+class Word
+  def initialize(input)
+    @input = input
+  end
+  def fetch
+    Datamuse.new.damu.send('words').get(params: {})
+
+
+
+CHARACTER_NAMES = [
+  "Arthur Dent",
+  "Ford Prefect",
+  "Tricia McMillan",
+  "Zaphod Beeblebrox",
+].freeze
+
+Readline.completion_proc = proc do |input|
+  Wrd_Methods.select { |name| name.start_with?(input) }
+end
+
+puts "Who's your favourite Hitchiker's Guide character?"
+input = Readline.readline("> ", false)
+if input
+  puts "You entered: #{input}"
+end
+
+
+
+ cmd = ask("Enter command:  ", %w[save sample load reset quit]) do |q|
+    q.readline = true
+  end
+  say("Executing \"#{cmd}\"...
+
+
+
+
+
+
+
+
+
+
+MenuOptions = OpenStruct.new(
+  start_over: {exec CLI.new.start}, exit: {exec exit}
+
+  )
+
+
+
+
+
+
+
+
+class CLI
+  extend Wrds
+
+  def self.start
+    puts Wrds::title
+    puts Wrds::slogan
+    puts Wrds::space
+    menu
+  end
+
+
+  def self.menu
+    puts Wrds::instructions
+    Display.options(Wrd_Methods)
+    
+  end
+end
 
 
 
